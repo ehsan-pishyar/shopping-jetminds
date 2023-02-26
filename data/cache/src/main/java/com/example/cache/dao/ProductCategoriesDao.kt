145cbc2aa3eaf2266dd9c43a5084d52e98ff1326
Ehsan Pishyar <ehsan.pishyar@gmail.com>
2023-02-26 23:10:25 +0330

    Added ProductCategoriesRepositoryImpl.kt

--- data/cache/src/main/java/com/example/cache/dao/ProductCategoriesDao.kt
+++ data/cache/src/main/java/com/example/cache/dao/ProductCategoriesDao.kt
@@ -1,12 +1,26 @@
 package com.example.cache.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.cache.models.ProductCategoriesResponseEntity
 
 @Dao
 interface ProductCategoriesDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertProductCategoriesToDb(categories: List<ProductCategoriesResponseEntity>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertProductCategoryDetails(categoryDetails: ProductCategoriesResponseEntity)
+
     @Query("SELECT * FROM `product_categories_table`")
     suspend fun fetchProductCategories(): List<ProductCategoriesResponseEntity>
+
+    @Query("SELECT * FROM `product_categories_table` WHERE id = :categoryId")
+    suspend fun fetchProductCategoryDetails(categoryId: Int): ProductCategoriesResponseEntity
+
+    @Query("SELECT COUNT(*) FROM `product_categories_table`")
+    suspend fun isProductCategoryCacheAvailable(): Int
 }
---
+++ data/repository/src/main/java/com/example/repository/repository/ProductCategoriesRepositoryImpl.kt
@@ -0,0 +1,71 @@
+package com.example.repository.repository
+
+import com.example.cache.dao.ProductCategoriesDao
+import com.example.cache.models.ProductCategoriesResponseEntity
+import com.example.domain.models.ProductCategoriesResponse
+import com.example.domain.repositories.ProductCategoriesRepository
+import com.example.domain.utils.ServiceResult
+import com.example.network.ApiService
+import com.example.network.models.ProductCategoriesResponseDto
+import com.example.repository.mappers.toDomain
+import com.example.repository.mappers.toEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.io.IOException
+import javax.inject.Inject
+
+class ProductCategoriesRepositoryImpl @Inject constructor(
+    private val apiService: ApiService,
+    private val productCategoriesDao: ProductCategoriesDao
+): ProductCategoriesRepository {
+
+    override fun getProductCategories(): Flow<ServiceResult<List<ProductCategoriesResponse>>> = flow {
+        emit(ServiceResult.Loading())
+
+        if (productCategoriesDao.isProductCategoryCacheAvailable() > 0) {
+            emit(ServiceResult.Success(data = productCategoriesDao.fetchProductCategories().map { it.toDomain() }))
+        }
+
+        val response = try {
+            apiService.getProductsCategories()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            emit(ServiceResult.Error(data = null, error = e.message))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(ServiceResult.Error(data = null, error = e.message))
+        } as ProductCategoriesResponseDto
+
+        response.let { categories ->
+            productCategoriesDao.insertProductCategoriesToDb(listOf(categories.toEntity()))
+            emit(ServiceResult.Success(productCategoriesDao.fetchProductCategories().map { it.toDomain() }))
+        }
+
+        emit(ServiceResult.Loading(isLoading = false))
+    }
+
+    override suspend fun getProductCategoryDetails(categoryId: Int): ServiceResult<ProductCategoriesResponse> {
+        val categoryEntity = productCategoriesDao.fetchProductCategoryDetails(categoryId)
+
+        if (productCategoriesDao.isProductCategoryCacheAvailable() > 0) {
+            return ServiceResult.Success(data = categoryEntity.toDomain())
+        }
+
+        val remoteCategoryDetails = try {
+            apiService.getProductCategoryById(categoryId)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return ServiceResult.Error(data = null, error = e.message)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return ServiceResult.Error(data = null, error = e.message)
+        }
+
+        remoteCategoryDetails.let {
+            productCategoriesDao.insertProductCategoryDetails(it.toEntity())
+            return ServiceResult.Success(productCategoriesDao.fetchProductCategoryDetails(categoryId).toDomain())
+        }
+    }
+
+
+}
