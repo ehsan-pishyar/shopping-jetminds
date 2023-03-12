4758d9962721351e6f29f42c8e0824cf04ac7db7
e.pishyar <ehsan.pishyar@gmail.com>
2023-03-12 16:42:34 +0330

    minor changes

--- app/src/main/java/com/example/shoppingjetminds/views/main/HomeScreen.kt
+++ app/src/main/java/com/example/shoppingjetminds/views/main/HomeScreen.kt
@@ -1,21 +1,21 @@
 package com.example.shoppingjetminds.views.main
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.shoppingjetminds.R
 import com.example.shoppingjetminds.ui.theme.Background
-import com.example.shoppingjetminds.viewmodels.ProductCategoriesViewModel
 
 @Composable
 fun HomeScreen(
-    productCategoriesViewModel: ProductCategoriesViewModel = hiltViewModel()
+    // productCategoriesViewModel: ProductCategoriesViewModel = hiltViewModel()
 ){
     Box(
         modifier = Modifier
@@ -24,7 +24,22 @@ fun HomeScreen(
             .background(Background)
     ) {
         Column(modifier = Modifier.fillMaxSize()) {
-
+            // Banner
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .height(230.dp)
+                .background(Color(0xFFf6f6f6))
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(painter = painterResource(id = R.drawable.image_placeholder), contentDescription = "main banner")
+                }
+            }
+            // Categories
+            // OffSale Products
         }
     }
 }
